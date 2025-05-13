0f1bb92035e7527cd9b6db90832f3e7cc77e5485
Manoj Bahadur Kunwar <mbkunwar@myseneca.ca>
2025-05-13 11:30:45 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program will print a greeting with a name'''
+
+# Define a string object
+name = 'Isaac'
+
+# Print greeting
+print('How old are you ' + name + '?')
