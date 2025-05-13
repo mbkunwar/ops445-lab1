5c7b333cff30ac7d06723ff7972cc627f1f9b53f
Manoj Bahadur Kunwar <mbkunwar@myseneca.ca>
2025-05-13 11:32:51 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program displays a person's name and age.'''
+
+# Define objects
+name = 'Isaac'
+age = 72
+
+# Print output combining string and converted integer
+print(name + ' is ' + str(age) + ' years old!')
