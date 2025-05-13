33d8665b1fdf7fcf1e60e497105c60411f681f81
Manoj Bahadur Kunwar <mbkunwar@myseneca.ca>
2025-05-13 11:37:00 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''This script demonstrates math operations using variables.'''
+
+# Define variables
+x = 10
+y = 2
+z = 5
+
+# Perform math using PEMDAS: 10 + 2 * 5 = 20
+result = x + y * z
+
+# Print the result in the required format
+print(f'{x} + {y} * {z} = {result}')
+
